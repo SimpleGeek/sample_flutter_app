058008e6a1b178816520e91eb8c72864cef1f5e8
Isaiah <isaiah@localhost.localdomain>
2020-05-09 17:20:47 -0400

    Changed to use stateful favorited widget

--- lib/main.dart
+++ lib/main.dart
@@ -38,12 +38,8 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          /* Using stateful widget for favorited icon and text */
+          FavoriteWidget(),
         ],
       ),
     );
@@ -121,5 +117,55 @@ class MyApp extends StatelessWidget {
         ),
       ], // Children
     );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  // Private state
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  // Private functions
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        // Remove a favorite from the count; update the display
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        // Add a favorite to the count; update the display
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  // Main widget build function
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ], // Children
+    ); // Row
   }
 }
